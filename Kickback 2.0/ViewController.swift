56fb43e9440e505612370abeed804cdc0929606c
Nelson <nelson@nelsons-macbook-pro.local>
2018-10-10 12:42:07 -0700

    Initial Commit

---
+++ Kickback 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kickback 2.0
+//
+//  Created by Nelson on 10/10/18.
+//  Copyright © 2018 Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
